4f6ac0b23009184ab850ded189e27384a29b1b9a
Alexei <alexei1090@gmail.com>
2022-06-16 19:51:37 +0300

    Program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+Console.Clear();
+
+string[] array = {"hello", "2", "world", ":-)"};
+Console.WriteLine(Met(array));
+
+
+string[] m = Met(array).Split(' ');
+Console.WriteLine(m);
+
+
+string Met(string[] arr)
+{
+    string st = "";
+    int i = 0;
+    int j = arr.Length - 1;
+    while(i<=j)
+    {
+        if(arr[i].Length <= 3)
+        {
+            st = st + $"{arr[i]} ";
+            i = i + 1;
+        }
+        else
+        {
+            i = i + 1;
+        }
+    }
+    return st;
+}
